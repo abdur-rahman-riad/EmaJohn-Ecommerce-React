296144d0327982d5cf2566704c4dfb0cdaa0a4a2
Mohammad Abdur Rahman <rahmanriad.cse@gmail.com>
2021-09-23 00:42:38 +0600

    Header Design Complete

---
+++ src/components/Header/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from "../../images/logo.png";
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="header">
+            {/* NavBar Design */}
+            <div className="nav-bar">
+                <div className="nav-logo">
+                    <img className="logo" src={logo} alt="Logo" />
+                </div>
+                <div className="nav-item">
+                    <a href="/shop">Shop</a>
+                    <a href="/orders">Order Review</a>
+                    <a href="/inventory">Manage Inventory</a>
+                </div>
+            </div>
+
+            {/* Search Option Design */}
+            <div className="search-option">
+                <div className="search-filed">
+                    <input type="text" placeholder="Search your products" />
+                    <button>Search</button>
+                </div>
+                <div className="cart-count">
+                    00
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
